a1086e87de8560eba2090f6f4583274e8e1a4f84
Krit B <gmkipladen@gmail.com>
2023-09-24 17:56:04 +0700

    feat(ui): add tailwind

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,2 +1,1 @@
 export * from './lib/typography/typography';
-export * from './lib/ui';
--- ui/src/lib/typography/typography.tsx
+++ ui/src/lib/typography/typography.tsx
@@ -1,13 +1,9 @@
-import styles from './typography.module.scss';
-
 /* eslint-disable-next-line */
 export interface TypographyProps {}
 
 export function Typography(props: TypographyProps) {
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to Typography!</h1>
-    </div>
+    <div className="bg-indigo-500 p-2 font-mono">Hello!</div>
   );
 }
 
