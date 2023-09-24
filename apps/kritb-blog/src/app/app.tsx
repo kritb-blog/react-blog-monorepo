9867162b3aabcf3cf6bbd127d187fafa6955ab75
Krit B <gmkipladen@gmail.com>
2023-09-24 17:19:56 +0700

    Initial commit

---
+++ apps/kritb-blog/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="kritb-blog" />
+    </div>
+  );
+}
+
+export default App;
